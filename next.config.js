e7c6b1ffcc61c4e14bcb06b868ab4b6ed6eeb62b
Saroj khadka <saroj.khadka@ekbana.info>
2024-04-02 22:41:03 +0545

    refactor: button test update and config update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
+  // Other configurations...
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'must-revalidate',
+          },
+        ],
+      },
+      // Add other routes as needed
+    ];
+  },
 };
--- pages/test2.js
+++ pages/test2.js
@@ -6,7 +6,7 @@ const Test2 = () => {
         <>
             <p> This is a test2 page</p>
             <Link href={"/test"}>
-                <button>Test</button>
+                <button> Vist Test</button>
             </Link>
         </>
     )
