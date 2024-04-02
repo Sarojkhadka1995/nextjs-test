14d9cbee7acf9fc1d0bdba36e9babbd1ade217e0
Saroj khadka <saroj.khadka@ekbana.info>
2024-04-02 22:36:33 +0545

    refactor: button test update

--- pages/test2.js
+++ pages/test2.js
@@ -6,7 +6,7 @@ const Test2 = () => {
         <>
             <p> This is a test2 page</p>
             <Link href={"/test"}>
-                <button>Goto Test</button>
+                <button>Test</button>
             </Link>
         </>
     )
