2f328d5b15bb9fe06839d1fa79e3c658dc4f1aa4
Saroj khadka <saroj.khadka@ekbana.info>
2024-04-02 23:59:46 +0545

    feat: revalidation test

--- pages/test.js
+++ pages/test.js
@@ -1,15 +1,29 @@
-import Link from "next/link"
+import Link from 'next/link';
 
+// async function fetchData() {
+//     // Simulate fetching data from an API or database
+//     const response = await fetch('https://api.example.com/data');
+//     const data = await response.json();
+//     return data;
+// }
+
+export async function getStaticProps() {
+    // No data fetching here (intentionally empty)
+    return {
+        props: {}, // Return an empty object
+        revalidate: 60, // Revalidate every 60 seconds (adjust as needed)
+    };
+}
 
 const Test = () => {
     return (
         <>
-            <p> This is a test page</p>
-            <Link href={"/test2"}>
-                <button>Click here to vist Test2</button>
+            <p>This is a test page</p>
+            <Link href="/test2">
+                <button>Click here to visit Test2</button>
             </Link>
         </>
-    )
-}
+    );
+};
 
-export default Test
+export default Test;
