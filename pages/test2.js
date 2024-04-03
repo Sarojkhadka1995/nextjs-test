1c8cc3eaf44b0d5864b8ebc3d5beb14d9d7481c9
Saroj khadka <saroj.khadka@ekbana.info>
2024-04-04 00:39:51 +0545

    button text update

--- pages/test2.js
+++ pages/test2.js
@@ -6,7 +6,7 @@ const Test2 = () => {
         <>
             <p> This is a test2 page</p>
             <Link href={"/test"}>
-                <button>TEST</button>
+                <button>TEST LINK</button>
             </Link>
         </>
     )
