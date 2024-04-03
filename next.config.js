7790dbbd6c1dab79e345889bee5ec77391e7869b
Saroj khadka <saroj.khadka@ekbana.info>
2024-04-03 17:49:38 +0545

    feat: log add to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
   // Other configurations...
   async headers() {
     return [
