93f14bbcac9dd608ef377d3cc6fca8ed682a7ba1
Saroj khadka <saroj.khadka@ekbana.info>
2024-04-03 22:51:18 +0545

    feat: revalidate test

--- pages/test.js
+++ pages/test.js
@@ -20,7 +20,7 @@ const Test = () => {
         <>
             <p>This is a test page</p>
             <Link href="/test2">
-                <button>Click here to vist Test2 page </button>
+                <button>Click to vist Test2  </button>
             </Link>
         </>
     );
