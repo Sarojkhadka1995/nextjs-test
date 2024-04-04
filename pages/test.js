917e7252c91b00071b149914f55325115490bfe9
saroj khadka <saroj.khadka@ekbana.info>
2024-04-04 10:52:07 +0545

    revert changes

--- pages/test.js
+++ pages/test.js
@@ -8,9 +8,9 @@ const Test = () => {
   return (
     <>
       <p>This is a test page</p>
-      <a href="/test2">
-        <button>TEST2 LINK TEST</button>
-      </a>
+      <Link href="/test2">
+        <button>TEST2</button>
+      </Link>
     </>
   );
 };
--- pages/test2.js
+++ pages/test2.js
@@ -4,9 +4,9 @@ const Test2 = () => {
   return (
     <>
       <p> This is a test2 page</p>
-      <a href={'/test'}>
-        <button>TEST LINK TEST</button>
-      </a>
+      <Link href={'/test'}>
+        <button>TEST</button>
+      </Link>
     </>
   );
 };
